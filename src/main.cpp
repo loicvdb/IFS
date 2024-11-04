56cd283712189192207fe18623667001b7336448
loicvdb <34895914+loicvdb@users.noreply.github.com>
2024-11-04 17:51:35 +0100

    Added render pass and command recording

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ void checkResult(VkResult result)
 }
 
 #define ASSERT_SUCCESS(res) checkResult(res)
+#define SWAPCHAIN_BUFFER_COUNT 3
 
 int main()
 {
@@ -81,6 +82,9 @@ int main()
     VkDevice device;
     ASSERT_SUCCESS(vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device));
 
+    VkQueue queue;
+    vkGetDeviceQueue(device, 0, 0, &queue);
+
     VmaAllocatorCreateInfo allocatorCreateInfo{};
     allocatorCreateInfo.instance = instance;
     allocatorCreateInfo.physicalDevice = physicalDevice;
@@ -89,6 +93,15 @@ int main()
 
     VmaAllocator allocator;
     ASSERT_SUCCESS(vmaCreateAllocator(&allocatorCreateInfo, &allocator));
+
+    VkCommandPoolCreateInfo commandPoolCreateInfo{};
+    commandPoolCreateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    commandPoolCreateInfo.pNext = nullptr;
+    commandPoolCreateInfo.flags = 0;
+    commandPoolCreateInfo.queueFamilyIndex = 0;
+
+    VkCommandPool commandPool;
+    ASSERT_SUCCESS(vkCreateCommandPool(device, &commandPoolCreateInfo, nullptr, &commandPool));
 
     const uint32_t width = 800;
     const uint32_t height = 600;
@@ -108,7 +121,7 @@ int main()
     swapchainCreateInfo.pNext = nullptr;
     swapchainCreateInfo.flags = 0;
     swapchainCreateInfo.surface = surface;
-    swapchainCreateInfo.minImageCount = 3;
+    swapchainCreateInfo.minImageCount = SWAPCHAIN_BUFFER_COUNT;
     swapchainCreateInfo.imageFormat = VK_FORMAT_B8G8R8A8_SRGB;
     swapchainCreateInfo.imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
     swapchainCreateInfo.imageExtent = swapchainExtent;
@@ -126,14 +139,229 @@ int main()
     VkSwapchainKHR swapchain;
     ASSERT_SUCCESS(vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &swapchain));
 
+    VkAttachmentDescription colorAttachment{};
+    colorAttachment.flags = 0;
+    colorAttachment.format = VK_FORMAT_B8G8R8A8_SRGB;
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference colorAttachmentReference{};
+    colorAttachmentReference.attachment = 0;
+    colorAttachmentReference.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass{};
+    subpass.flags = 0;
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.inputAttachmentCount = 0;
+    subpass.pInputAttachments = nullptr;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorAttachmentReference;
+    subpass.pResolveAttachments = nullptr;
+    subpass.pDepthStencilAttachment = nullptr;
+    subpass.preserveAttachmentCount = 0;
+    subpass.pPreserveAttachments = nullptr;
+
+    // https://github.com/KhronosGroup/Vulkan-Docs/wiki/Synchronization-Examples-(Legacy-synchronization-APIs)#swapchain-image-acquire-and-present
+    VkSubpassDependency acquireToAttachmentDependency{};
+    acquireToAttachmentDependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+    acquireToAttachmentDependency.dstSubpass = 0;
+    acquireToAttachmentDependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    acquireToAttachmentDependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    acquireToAttachmentDependency.srcAccessMask = 0;
+    acquireToAttachmentDependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+    acquireToAttachmentDependency.dependencyFlags = 0;
+
+    VkRenderPassCreateInfo renderPassCreateInfo{};
+    renderPassCreateInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderPassCreateInfo.pNext = nullptr;
+    renderPassCreateInfo.flags = 0;
+    renderPassCreateInfo.attachmentCount = 1;
+    renderPassCreateInfo.pAttachments = &colorAttachment;
+    renderPassCreateInfo.subpassCount = 1;
+    renderPassCreateInfo.pSubpasses = &subpass;
+    renderPassCreateInfo.dependencyCount = 1;
+    renderPassCreateInfo.pDependencies = &acquireToAttachmentDependency;
+
+    VkRenderPass renderPass{};
+    ASSERT_SUCCESS(vkCreateRenderPass(device, &renderPassCreateInfo, nullptr, &renderPass));
+
+    VkImage swapchainImages[SWAPCHAIN_BUFFER_COUNT];
+    VkImageView swapchainImageViews[SWAPCHAIN_BUFFER_COUNT];
+    VkFramebuffer swapchainFramebuffers[SWAPCHAIN_BUFFER_COUNT];
+
+    uint32_t swapchainImageCount = SWAPCHAIN_BUFFER_COUNT;
+    ASSERT_SUCCESS(vkGetSwapchainImagesKHR(device, swapchain, &swapchainImageCount, swapchainImages));
+
+    assert(swapchainImageCount == SWAPCHAIN_BUFFER_COUNT);
+
+    for (uint32_t i = 0; i < SWAPCHAIN_BUFFER_COUNT; i++)
+    {
+        VkImageSubresourceRange subresourceRange{};
+        subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        subresourceRange.baseMipLevel = 0;
+        subresourceRange.levelCount = 1;
+        subresourceRange.baseArrayLayer = 0;
+        subresourceRange.layerCount = 1;
+
+        VkImageViewCreateInfo imageViewCreateInfo{};
+        imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        imageViewCreateInfo.pNext = nullptr;
+        imageViewCreateInfo.flags = 0;
+        imageViewCreateInfo.image = swapchainImages[i];
+        imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        imageViewCreateInfo.format = VK_FORMAT_B8G8R8A8_SRGB;
+        imageViewCreateInfo.components = {};
+        imageViewCreateInfo.subresourceRange = subresourceRange;
+
+        ASSERT_SUCCESS(vkCreateImageView(device, &imageViewCreateInfo, nullptr, &swapchainImageViews[i]));
+
+        VkFramebufferCreateInfo framebufferCreateInfo{};
+        framebufferCreateInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        framebufferCreateInfo.pNext = nullptr;
+        framebufferCreateInfo.flags = 0;
+        framebufferCreateInfo.renderPass = renderPass;
+        framebufferCreateInfo.attachmentCount = 1;
+        framebufferCreateInfo.pAttachments = &swapchainImageViews[i];
+        framebufferCreateInfo.width = width;
+        framebufferCreateInfo.height = height;
+        framebufferCreateInfo.layers = 1;
+
+        ASSERT_SUCCESS(vkCreateFramebuffer(device, &framebufferCreateInfo, nullptr, &swapchainFramebuffers[i]));
+    }
+
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+
+        VkSemaphoreCreateInfo semaphoreCreateInfo{};
+        semaphoreCreateInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+        semaphoreCreateInfo.pNext = nullptr;
+        semaphoreCreateInfo.flags = 0;
+
+        VkSemaphore acquireSemaphore;
+        ASSERT_SUCCESS(vkCreateSemaphore(device, &semaphoreCreateInfo, nullptr, &acquireSemaphore));
+
+        VkSemaphore presentSemaphore;
+        ASSERT_SUCCESS(vkCreateSemaphore(device, &semaphoreCreateInfo, nullptr, &presentSemaphore));
+
+        uint32_t imageIndex;
+        ASSERT_SUCCESS(vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, acquireSemaphore, nullptr, &imageIndex));
+
+        VkCommandBufferAllocateInfo commandBufferAllocateInfo{};
+        commandBufferAllocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        commandBufferAllocateInfo.pNext = nullptr;
+        commandBufferAllocateInfo.commandPool = commandPool;
+        commandBufferAllocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        commandBufferAllocateInfo.commandBufferCount = 1;
+
+        VkCommandBuffer commandBuffer;
+        ASSERT_SUCCESS(vkAllocateCommandBuffers(device, &commandBufferAllocateInfo, &commandBuffer));
+
+        VkCommandBufferBeginInfo commandBufferBeginInfo{};
+        commandBufferBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        commandBufferBeginInfo.pNext = nullptr;
+        commandBufferBeginInfo.flags = 0;
+        commandBufferBeginInfo.pInheritanceInfo = nullptr;
+
+        ASSERT_SUCCESS(vkBeginCommandBuffer(commandBuffer, &commandBufferBeginInfo));
+
+        VkRect2D renderArea{};
+        renderArea.offset.x = 0;
+        renderArea.offset.y = 0;
+        renderArea.extent.width = width;
+        renderArea.extent.height = height;
+
+        VkClearValue clearValue{};
+        clearValue.color.float32[0] = 0.0f;
+        clearValue.color.float32[1] = 0.0f;
+        clearValue.color.float32[2] = 0.0f;
+        clearValue.color.float32[3] = 0.0f;
+
+        VkRenderPassBeginInfo renderPassBeginInfo{};
+        renderPassBeginInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+        renderPassBeginInfo.pNext = nullptr;
+        renderPassBeginInfo.renderPass = renderPass;
+        renderPassBeginInfo.framebuffer = swapchainFramebuffers[imageIndex];
+        renderPassBeginInfo.renderArea = renderArea;
+        renderPassBeginInfo.clearValueCount = 1;
+        renderPassBeginInfo.pClearValues = &clearValue;
+
+        vkCmdBeginRenderPass(commandBuffer, &renderPassBeginInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+        vkCmdSetScissor(commandBuffer, 0, 1, &renderArea);
+
+        VkViewport viewport{};
+        viewport.x = 0.0f;
+        viewport.y = 0.0f;
+        viewport.width = static_cast<float>(width);
+        viewport.height = static_cast<float>(height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+
+        vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+        vkCmdEndRenderPass(commandBuffer);
+
+        ASSERT_SUCCESS(vkEndCommandBuffer(commandBuffer));
+
+        VkPipelineStageFlags waitDstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.pNext = nullptr;
+        submitInfo.waitSemaphoreCount = 1;
+        submitInfo.pWaitSemaphores = &acquireSemaphore;
+        submitInfo.pWaitDstStageMask = &waitDstStageMask;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &commandBuffer;
+        submitInfo.signalSemaphoreCount = 1;
+        submitInfo.pSignalSemaphores = &presentSemaphore;
+
+        ASSERT_SUCCESS(vkQueueSubmit(queue, 1, &submitInfo, nullptr));
+
+        VkResult presentResult;
+
+        VkPresentInfoKHR presentInfo{};
+        presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+        presentInfo.pNext = nullptr;
+        presentInfo.waitSemaphoreCount = 1;
+        presentInfo.pWaitSemaphores = &presentSemaphore;
+        presentInfo.swapchainCount = 1;
+        presentInfo.pSwapchains = &swapchain;
+        presentInfo.pImageIndices = &imageIndex;
+        presentInfo.pResults = &presentResult;
+
+        ASSERT_SUCCESS(vkQueuePresentKHR(queue, &presentInfo));
+
+        ASSERT_SUCCESS(presentResult);
+
+        ASSERT_SUCCESS(vkQueueWaitIdle(queue));
+
+        vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+        vkDestroySemaphore(device, presentSemaphore, nullptr);
+        vkDestroySemaphore(device, acquireSemaphore, nullptr);
     }
+
+    for (uint32_t i = 0; i < SWAPCHAIN_BUFFER_COUNT; i++)
+    {
+        vkDestroyImageView(device, swapchainImageViews[i], nullptr);
+
+        vkDestroyFramebuffer(device, swapchainFramebuffers[i], nullptr);
+    }
+
+    vkDestroyRenderPass(device, renderPass, nullptr);
 
     vkDestroySwapchainKHR(device, swapchain, nullptr);
 
     vkDestroySurfaceKHR(instance, surface, nullptr);
+
+    vkDestroyCommandPool(device, commandPool, nullptr);
 
     vmaDestroyAllocator(allocator);
 
