4e1d25f864a2a00527f28294854e0bfea385447e
loicvdb <34895914+loicvdb@users.noreply.github.com>
2024-11-04 16:18:42 +0100

    Created instance, device, allocator, surface and swapchain

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,141 @@
 
 #include <iostream>
 
+void checkResult(VkResult result)
+{
+    assert(result == VK_SUCCESS);
+}
+
+#define ASSERT_SUCCESS(res) checkResult(res)
+
 int main()
 {
     glfwInit();
 
+    uint32_t instanceExtensionCount;
+    const char** instanceExtensionNames = glfwGetRequiredInstanceExtensions(&instanceExtensionCount);
+
+    VkApplicationInfo applicationInfo{};
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pNext = nullptr;
+    applicationInfo.pApplicationName = "IFS";
+    applicationInfo.applicationVersion = VK_MAKE_API_VERSION(0, 0, 0, 0);
+    applicationInfo.pEngineName = "";
+    applicationInfo.engineVersion = VK_MAKE_API_VERSION(0, 0, 0, 0);
+    applicationInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo instanceCreateInfo{};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pNext = nullptr;
+    instanceCreateInfo.flags = 0;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+    instanceCreateInfo.enabledLayerCount = 0;
+    instanceCreateInfo.ppEnabledLayerNames = nullptr;
+    instanceCreateInfo.enabledExtensionCount = instanceExtensionCount;
+    instanceCreateInfo.ppEnabledExtensionNames = instanceExtensionNames;
+
+    VkInstance instance;
+    ASSERT_SUCCESS(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
+
+    VkPhysicalDevice physicalDevice;
+    uint32_t physicalDeviceCount = 1;
+    VkResult enumeratePhysicalDevicesRes = vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, &physicalDevice);
+
+    assert(enumeratePhysicalDevicesRes == VK_SUCCESS || enumeratePhysicalDevicesRes == VK_INCOMPLETE);
+    assert(physicalDeviceCount > 0);
+
+    float queuePriority = 1.0f;
+
+    VkDeviceQueueCreateInfo queueCreateInfo{};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.pNext = nullptr;
+    queueCreateInfo.flags = 0;
+    queueCreateInfo.queueFamilyIndex = 0;   // should get the graphics/present queue on any GPU
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    const uint32_t deviceExtensionCount = 1;
+    const char* deviceExtensionNames[deviceExtensionCount]
+    {
+        "VK_KHR_swapchain",
+    };
+
+    VkPhysicalDeviceFeatures enabledFeatures{};
+
+    VkDeviceCreateInfo deviceCreateInfo{};
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    deviceCreateInfo.pNext = nullptr;
+    deviceCreateInfo.flags = 0;
+    deviceCreateInfo.queueCreateInfoCount = 1;
+    deviceCreateInfo.pQueueCreateInfos = &queueCreateInfo;
+    deviceCreateInfo.enabledLayerCount = 0;
+    deviceCreateInfo.ppEnabledLayerNames = nullptr;
+    deviceCreateInfo.enabledExtensionCount = deviceExtensionCount;
+    deviceCreateInfo.ppEnabledExtensionNames = deviceExtensionNames;
+    deviceCreateInfo.pEnabledFeatures = &enabledFeatures;
+
+    VkDevice device;
+    ASSERT_SUCCESS(vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device));
+
+    VmaAllocatorCreateInfo allocatorCreateInfo{};
+    allocatorCreateInfo.instance = instance;
+    allocatorCreateInfo.physicalDevice = physicalDevice;
+    allocatorCreateInfo.device = device;
+    allocatorCreateInfo.vulkanApiVersion = VK_API_VERSION_1_0;
+
+    VmaAllocator allocator;
+    ASSERT_SUCCESS(vmaCreateAllocator(&allocatorCreateInfo, &allocator));
+
+    const uint32_t width = 800;
+    const uint32_t height = 600;
+
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(width, height, "Vulkan window", nullptr, nullptr);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    VkSurfaceKHR surface;
+    ASSERT_SUCCESS(glfwCreateWindowSurface(instance, window, nullptr, &surface));
 
-    std::cout << extensionCount << " extensions supported\n";
+    VkExtent2D swapchainExtent{};
+    swapchainExtent.width = width;
+    swapchainExtent.height = height;
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+    VkSwapchainCreateInfoKHR swapchainCreateInfo{};
+    swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    swapchainCreateInfo.pNext = nullptr;
+    swapchainCreateInfo.flags = 0;
+    swapchainCreateInfo.surface = surface;
+    swapchainCreateInfo.minImageCount = 3;
+    swapchainCreateInfo.imageFormat = VK_FORMAT_B8G8R8A8_SRGB;
+    swapchainCreateInfo.imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
+    swapchainCreateInfo.imageExtent = swapchainExtent;
+    swapchainCreateInfo.imageArrayLayers = 1;
+    swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    swapchainCreateInfo.queueFamilyIndexCount = 0;
+    swapchainCreateInfo.pQueueFamilyIndices = nullptr;
+    swapchainCreateInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+    swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    swapchainCreateInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+    swapchainCreateInfo.clipped = VK_TRUE;
+    swapchainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
+
+    VkSwapchainKHR swapchain;
+    ASSERT_SUCCESS(vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &swapchain));
 
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
     }
+
+    vkDestroySwapchainKHR(device, swapchain, nullptr);
+
+    vkDestroySurfaceKHR(instance, surface, nullptr);
+
+    vmaDestroyAllocator(allocator);
+
+    vkDestroyDevice(device, nullptr);
+
+    vkDestroyInstance(instance, nullptr);
 
     glfwDestroyWindow(window);
 
